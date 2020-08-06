e3f1236d719a65d6e4ff98fd9235ee15b8279956
Valentin Piho <val.piho@gmail.com>
2020-08-06 22:21:26 +0300

    Installing ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
-import {RouterModule, Routes} from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 const routs: Routes = [
   {path: 'search/:keyword', component: ProductListComponent},
@@ -31,7 +32,8 @@ const routs: Routes = [
   imports: [
     RouterModule.forRoot(routs),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
